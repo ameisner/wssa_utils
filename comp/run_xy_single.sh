11ba2da2cffc498ccd785e22c34a4fee3f0e332c
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-10 23:15:49 -0500

    changed call to compare_outputs subroutine

--- comp/run_xy_single.sh
+++ comp/run_xy_single.sh
@@ -1,3 +1,3 @@
 echo -e .COM comp "\n" test_xy_single, \"coords_i.fits\" |idl &> rxysi.log
 echo -e import comp "\n"comp.test_xy_single\(\"coords_p.fits\"\) |python &> rxysp.log
-echo -e .COM comp "\n" compare_xy_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl &> rxys.log
+echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl &> rxys.log

2aa4a70f3b9326cd4fbeca84056c3d3d050c1e85
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-11 01:42:33 -0500

    redirect to common log file

--- comp/run_xy_single.sh
+++ comp/run_xy_single.sh
@@ -1,3 +1,3 @@
-echo -e .COM comp "\n" test_xy_single, \"coords_i.fits\" |idl &> rxysi.log
-echo -e import comp "\n"comp.test_xy_single\(\"coords_p.fits\"\) |python &> rxysp.log
-echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl &> rxys.log
+echo -e .COM comp "\n" test_xy_single, \"coords_i.fits\" |idl &> rxys.log
+echo -e import comp "\n"comp.test_xy_single\(\"coords_p.fits\"\) |python >> rxys.log 2>&1
+echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxys.log 2>&1

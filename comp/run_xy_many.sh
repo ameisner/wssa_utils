0c9634866c73caead10a3f3507b3596279032cbb
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-10 23:18:18 -0500

    changed call to compare_outputs subroutine

--- comp/run_xy_many.sh
+++ comp/run_xy_many.sh
@@ -1,3 +1,3 @@
 echo -e .COM comp "\n" test_xy_many, \"coords_i.fits\" |idl &> rxymi.log
 echo -e import comp "\n"comp.test_xy_many\(\"coords_p.fits\"\) |python &> rxymp.log
-echo -e .COM comp "\n" compare_xy_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl &> rxym.log
+echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl &> rxym.log

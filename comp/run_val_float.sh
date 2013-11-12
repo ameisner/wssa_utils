2896badba9c209c3c0f3955aa69a5d95acc75a2e
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-11 20:10:33 -0500

    runner to test Python vs IDL outputs for single-precision input coordinates

---
+++ comp/run_val_float.sh
@@ -0,0 +1,3 @@
+echo -e .COM comp "\n" test_val_float, \"val_i.fits\" |idl &> rvf.log
+echo -e import comp "\n"comp.test_val_float\(\"val_p.fits\"\) |python >> rvf.log 2>&1
+echo -e .COM comp "\n" compare_outputs, \"val_p.fits\", \"val_i.fits\" |idl >> rvf.log 2>&1

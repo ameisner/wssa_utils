320dc20ae9db3e040178806e3edaadfb159b9988
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-13 03:30:18 -0500

    clean up

--- comp/run_xy_many.sh
+++ comp/run_xy_many.sh
@@ -1,3 +1,2 @@
-echo -e .COM comp "\n" test_xy_many, \"coords_i.fits\" |idl &> rxym.log
-echo -e import comp "\n"comp.test_xy_many\(\"coords_p.fits\"\) |python >> rxym.log 2>&1
-echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxym.log 2>&1
+echo -e import comp "\n"comp.test_xy_many\(\"coords_p.fits\"\) |python &> rxym.log
+echo -e .COM comp "\n" test_xy_many, \"coords_i.fits\" \& compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxym.log 2>&1

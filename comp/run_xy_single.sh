ec489796970b03849abc546b18fd042be64e0179
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-13 03:31:53 -0500

    clean up

--- comp/run_xy_single.sh
+++ comp/run_xy_single.sh
@@ -1,3 +1,2 @@
-echo -e .COM comp "\n" test_xy_single, \"coords_i.fits\" |idl &> rxys.log
-echo -e import comp "\n"comp.test_xy_single\(\"coords_p.fits\"\) |python >> rxys.log 2>&1
-echo -e .COM comp "\n" compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxys.log 2>&1
+echo -e import comp "\n"comp.test_xy_single\(\"coords_p.fits\"\) |python &> rxys.log 
+echo -e .COM comp "\n" test_xy_single, \"coords_i.fits\" \& compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxys.log 2>&1

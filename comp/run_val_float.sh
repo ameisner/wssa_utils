d014baec6b2f4af7a6634b01ab29c7de09cb0125
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-13 03:26:16 -0500

    clean up

--- comp/run_val_float.sh
+++ comp/run_val_float.sh
@@ -1,3 +1,2 @@
-echo -e .COM comp "\n" test_val_float, \"val_i.fits\" |idl &> rvf.log
-echo -e import comp "\n"comp.test_val_float\(\"val_p.fits\"\) |python >> rvf.log 2>&1
-echo -e .COM comp "\n" compare_outputs, \"val_p.fits\", \"val_i.fits\" |idl >> rvf.log 2>&1
+echo -e import comp "\n"comp.test_val_float\(\"val_p.fits\"\) |python &> rvf.log
+echo -e .COM comp "\n" test_val_float, \"val_i.fits\" \& compare_outputs, \"val_p.fits\", \"val_i.fits\" |idl >> rvf.log 2>&1

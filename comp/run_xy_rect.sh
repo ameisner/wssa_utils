989c34d3fd9b3e599f268c74c168ad28bde9ef1c
Aaron Meisner <ameisner@fas.harvard.edu>
2013-11-17 16:51:08 -0500

    automatically display relevant portion of log file

--- comp/run_xy_rect.sh
+++ comp/run_xy_rect.sh
@@ -1,2 +1,6 @@
 echo -e import comp "\n"comp.test_xy_rect\(\"coords_p.fits\"\) |python &> rxyr.log
 echo -e .COM comp "\n" test_xy_rect, \"coords_i.fits\" \& compare_outputs, \"coords_p.fits\", \"coords_i.fits\" |idl >> rxyr.log 2>&1
+tot=`wc -l < rxyr.log`
+sta=`sed -n '/checking/=' rxyr.log |head -1`
+nta=$(( ${tot}-${sta}+1 ))
+tail -$nta rxyr.log
